ba5a9b10dcb4b93edeb3c5ae57755f842bee2ba0
carlosagonzalez246 <carlosagonzalez246@gmail.com>
2021-05-16 00:42:24 -0500

    Updated the Java Code to test the Jenkins / Docker CI/CD

--- listofstrings.java
+++ listofstrings.java
@@ -1,5 +1,6 @@
 public class listofstrings{
     public static void main(String[] args) {
                         System.out.format("This is the first string.%n");
+			System.out.format("This is the second string.%n");
         }
 }
