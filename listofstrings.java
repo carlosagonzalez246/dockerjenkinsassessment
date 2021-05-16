6f955ffcbaad787b8dd7d937f44c2c1fa1371f0e
carlosagonzalez246 <carlosagonzalez246@gmail.com>
2021-05-16 01:48:53 -0500

    Java code update to start CI/CD

--- listofstrings.java
+++ listofstrings.java
@@ -2,5 +2,6 @@ public class listofstrings{
     public static void main(String[] args) {
                         System.out.format("This is the first string.%n");
 			System.out.format("This is the second string.%n");
+                        System.out.format("This is the third string.%n");
         }
 }
